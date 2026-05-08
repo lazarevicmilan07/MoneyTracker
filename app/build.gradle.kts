f221dc1361839192973a09d3c043c6581270ee58
Milan Lazarević <milan.lazarevic@comdata.rs>
2026-05-08 19:16:38 +0200

    version code bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.moneytracker.simplebudget"
         minSdk = 26
         targetSdk = 35
-        versionCode = 17
-        versionName = "3.3.0"
+        versionCode = 18
+        versionName = "3.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
